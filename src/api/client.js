7d3cb70baee4b4bea7784cd9ae5308617c6094f2
siyeon-05 <fairysyl@naver.com>
2026-01-23 16:39:16 +0900

    update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 ﻿import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL?.replace(/\/+$/, "") || "";
+  import.meta.env.VITE_API_BASE_URL?.replace(/\/+$/, "") ||
+  "http://localhost:8080";
 
 const TOKEN_KEY = "accessToken";
 
